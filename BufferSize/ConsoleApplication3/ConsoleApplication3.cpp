5fa6d202fda3838f61a566575e4fd71595f83588
217117 <217117@KB37-302-C06>
2024-10-11 10:02:53 +0300

    4 задачи с буфером

---
+++ BufferSize/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,21 @@
+﻿#include <Windows.h>
+#include <iostream>
+int main()
+{
+	COORD coord; // для размера буфера экрана
+	HANDLE hStd0ut; // дескриптор стандартного вывода
+	// читаем дескриптор стандартного вывода
+	hStd0ut = GetStdHandle(STD_OUTPUT_HANDLE);
+	// вводим новый размер буфера экрана
+	std::cout << "Enter new screen buffer size." << std::endl;
+	std::cout << "A number of columns: ";
+	std::cin >> coord.X;
+	std::cout << "A number of rows: ";
+	std::cin >> coord.Y;
+	// устанавливаем новый размер буфера экрана
+	if (!SetConsoleScreenBufferSize(hStd0ut, coord))
+	{
+		std::cout << "Set console screen buffer size failed." << std::endl; return GetLastError();
+	}
+	return 0;
+}
